3fcaa6f733dad282f16d206eca36958b15189967
melabreu <melissa.abreu84@gmail.com>
2025-11-07 18:00:42 -0500

    Final Heroku static server setup

---
+++ portfolio/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const app = express();
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
