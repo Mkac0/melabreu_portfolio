6669f44156b6d5e9e060a240edbd1adb384a2822
melabreu <melissa.abreu84@gmail.com>
2025-11-07 17:41:21 -0500

    education icon progress

--- portfolio/src/data/EducationData.js
+++ portfolio/src/data/EducationData.js
@@ -1,14 +1,22 @@
+import GALogo from '../assets/logos/ga.png'; 
+import QCLogo from '../assets/logos/qc.png';
+import BMCCLogo from '../assets/logos/bmcc.png';
+import GoogleLogo from '../assets/logos/google.png';
+import ATPLogo from '../assets/logos/la-guardia.png';
+
 export const education = [
   {
     degree: "Advance Software Engineering Immersive Program",
     institution: "General Assembly",
     date: "Oct 2025",
     details: ["Built robust back-end applications using Node.js/Express, Python/Django, SQL/NoSQL databases, and trained in MVC, REST APIs, and Git/GitHub workflow."], 
+    logo: GALogo,
   },
   {
     degree: "Bachelor of Arts in Economics",
     institution: "Queens College, City University of New York",
     date: "May 2020",
+    logo: QCLogo,
     details: [
       "Related Coursework: Price Theory, Econometrics, Business & Economics, Statistics",
       "Member, Alliance of Latin American Students (ALAS)",
@@ -21,6 +29,7 @@ export const education = [
     details: [
       "Member, Freshman Learning Academy (FLA)",
     ],
+    logo: BMCCLogo,
   },
 ];
 
@@ -28,10 +37,12 @@ export const certifications = [
   { 
     name: "Google Foundation of User Experience (UX) Design", 
     date: "Jun 2024" ,
+    logo: GoogleLogo,
     link: "https://www.coursera.org/account/accomplishments/verify/KQEDVTZ8HA4P"
   },
   { 
     name: "Accredited Tax Preparer (ATP)", 
-    date: "May 2019" 
+    date: "May 2019" ,
+    logo: ATPLogo
   },
 ];
